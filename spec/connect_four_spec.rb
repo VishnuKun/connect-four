d7ad39f35e96c8a2a98370ac2a2ef797a67eb11e
Vishnu <vishnukun954AV@protonmail.com>
2023-09-06 21:21:59 +0530

    Added test for game_over? method

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -8,7 +8,7 @@ describe ConnectFour do
     subject(:game_board) { described_class.new }
     context 'when invoked' do
       it 'should display the board' do
-        board = <<-Board
+        board = <<-BOARD
         Current board status :-
 
         | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
@@ -20,17 +20,17 @@ describe ConnectFour do
         | + | + | + | + | + | + | + |
         | + | + | + | + | + | + | + |
         |---|---|---|---|---|---|---|
-        Board
+        BOARD
         expect(game_board).to receive(:puts).with(board)
         game_board.display_board
       end
     end
   end
 
-  describe "#place_piece" do 
+  describe '#place_piece' do
     subject(:game_piece) { described_class.new }
-    context "when given 1st gate" do 
-      it 'should place the piece(●) correctly on 1st column last line' do 
+    context 'when given 1st gate' do
+      it 'should place the piece(●) correctly on 1st column last line' do
         board = game_piece.instance_variable_get(:@board)
         column = 1
         piece = '●'
@@ -40,8 +40,8 @@ describe ConnectFour do
       end
     end
 
-    context "when given 4th gate" do 
-      it 'should place the piece(○) correctly on 4th column 3rd line' do 
+    context 'when given 4th gate' do
+      it 'should place the piece(○) correctly on 4th column 3rd line' do
         board = game_piece.instance_variable_get(:@board)
         column = 4
         piece = '○'
@@ -52,29 +52,92 @@ describe ConnectFour do
     end
   end
 
-  describe "#verify" do
-    subject(:game_verify) { described_class.new } 
-    context "when given invalid column" do 
-      it "returns nil" do
+  describe '#verify' do
+    subject(:game_verify) { described_class.new }
+    context 'when given invalid column' do
+      it 'returns nil' do
         invalid_column = 9
         result = game_board.verify(invalid_column)
         expect(result).to eq nil
       end
     end
 
-    context "when given invalid column" do 
-      it "returns nil" do
+    context 'when given invalid column' do
+      it 'returns nil' do
         invalid_column = 'Q'
         result = game_board.verify(invalid_column)
         expect(result).to eq nil
       end
     end
 
-    context "when given valid column" do 
-      it "returns given column" do
+    context 'when given valid column' do
+      it 'returns given column' do
         valid_column = 5
         result = game_board.verify(valid_column)
         expect(result).to eq valid_column
+      end
+    end
+  end
+
+  describe '#game_over?' do
+    subject(:game_over) { described_class.new }
+    context "when method finds winning pattern in first column" do
+      it "returns true" do
+        board = [
+          ['+', '●', '●', '●', '●', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+']
+        ]
+        expect(game_board.game_over?(board)).to be true
+      end
+    end
+
+    context "when method finds winning pattern across column 3 & 6 horizontally" do
+      it "returns true" do
+        board = [
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '●', '+', '+', '+'],
+          ['+', '+', '●', '+', '+', '+'],
+          ['+', '+', '●', '+', '+', '+'],
+          ['+', '+', '●', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+']
+        ]
+        expect(game_board.game_over?(board)).to be true
+      end
+    end
+
+    context "when method finds winning pattern across column 1 & 4 diagonally" do
+      it "returns true" do
+        board = [
+          ['+', '●', '+', '+', '+', '+'],
+          ['+', '+', '●', '+', '+', '+'],
+          ['+', '+', '+', '●', '+', '+'],
+          ['+', '+', '+', '+', '●', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+']
+        ]
+        expect(game_board.game_over?(board)).to be true
+      end
+    end
+
+    context "when method finds no winning pattern " do
+      it "returns false" do
+        board = [
+          ['+', '●', '+', '+', '+', '+'],
+          ['+', '+', '○', '+', '+', '+'],
+          ['+', '+', '+', '●', '+', '+'],
+          ['+', '+', '+', '○', '○', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+'],
+          ['+', '+', '+', '+', '+', '+']
+        ]
+        expect(game_board.game_over?(board)).to be false
       end
     end
   end
