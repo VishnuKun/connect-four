072867ecbcc7d55587420c2fb9fe05c34ec488ea
Vishnu <vishnukun954AV@protonmail.com>
2023-09-06 20:11:50 +0530

    Added test for place_piece method

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -26,4 +26,29 @@ describe ConnectFour do
       end
     end
   end
+
+  describe "#place_piece" do 
+    subject(:game_piece) { described_class.new }
+    context "when given 1st gate" do 
+      it 'should place the piece(●) correctly on 1st column last line' do 
+        board = game_piece.instance_variable_get(:@board)
+        column = 1
+        piece = '●'
+        target = board[column - 1][-1]
+        expect(target).to eq(piece)
+        game_board.place_piece(column, piece)
+      end
+    end
+
+    context "when given 4th gate" do 
+      it 'should place the piece(○) correctly on 4th column 3rd line' do 
+        board = game_piece.instance_variable_get(:@board)
+        column = 4
+        piece = '○'
+        target = board[column - 1][2]
+        expect(target).to eq(piece)
+        game_board.place_piece(column, piece)
+      end
+    end
+  end
 end
