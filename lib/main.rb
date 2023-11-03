a3efb9a198518604c2e1fffa8cf761e2aa5be9f6
Vishnu <vishnukun954AV@protonmail.com>
2023-11-03 20:16:55 +0530

    Uncommented and removed comments

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,4 @@
 require_relative 'connect_four'
-# to start the game uncomment the lines below
-# and run main.rb in the terminal
-# game = ConnectFour.new
-# game.play_game
+# to start the game run main.rb in the terminal
+game = ConnectFour.new
+game.play_game
